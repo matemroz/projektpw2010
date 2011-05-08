1ed7a46a6c5af433b763b62f399b1b7fa7a6d6de
bieniek.greg@gmail.com <bieniek.greg@gmail.com@8acf965e-a676-820a-5892-ec6800c534e3>
2011-05-08 10:35:14 +0000

    dodano interfejsy 'familymanager' do obslugi komunikacji serwer-BD

---
+++ trunk/ChildControlServerApp/src/pw/childcontrol/server/familymanager/IAreaManager.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package pw.childcontrol.server.familymanager;
+
+import java.util.List;
+
+public interface IAreaManager {
+	//### AREA ###
+	/**
+	 * Pobiera listę niebezpiecznych miejsc zdefiniowanych dla konkretnego dziecka.
+	 * 
+	 * @param idChild indywidualny numer dziecka
+	 * @return lista niebezpiecznych miejsc
+	 */
+	public List<Area> getDangerousAreas(int idChild);
+	
+	/**
+	 * Dodaje niebezpieczne miejsca dla konkretnego dziecka.
+	 * 
+	 * @param idChild indywidualny numer dziecka
+	 * @param area niebezpieczne miejsce
+	 * @return prawda - jeśli operacja się udała, fałsz - jeśli zakończono niepowodzeniem
+	 */
+	public boolean addDangerousArea(int idChild, Area area);
+	
+	/**
+	 * Usuwa niebezpieczne miejsce zdefiniowane dla konkretnego dziecka.
+	 * 
+	 * @param idChild indywidualny numer dziecka
+	 * @param area niebezpieczne miejsce
+	 * @return prawda - jeśli operacja się udała, fałsz - jeśli zakończono niepowodzeniem
+	 */
+	public boolean removeDangerousArea(int idChild, Area area);
+	//###
+	
+	//### AREA POINT ###
+	/**
+	 * Sprawdza czy punkt geograficzny należy do obszaru zdefiniowanego jako niebezpieczny
+	 * dla konkretnego dziecka.
+	 * 
+	 * @param idChild indywidualny numer dziecka
+	 * @param point punkt geograficzny
+	 * @return prawda - jeśli punkt jest niebezpieczny, fałsz - jesli punkt jest bezpieczny
+	 */
+	public boolean idDangerousArea(int idChild, AreaPoint point);
+	//###
+}
---
+++ trunk/ChildControlServerApp/src/pw/childcontrol/server/familymanager/IChild.java
@@ -0,0 +1,139 @@
+/**
+ * 
+ */
+package pw.childcontrol.server.familymanager;
+
+import java.util.Date;
+import java.util.List;
+
+public interface IChild {
+	//### LOCATION ###
+	/**
+	 * Znajduje aktualne położenie geograficzne dziecka.
+	 * 
+	 * @param idChild indywidualny numer dziecka
+	 * @return aktualne położenie geograficzne
+	 */
+	public AreaPoint getLocation(int idChild);
+	
+	/**
+	 * Ustawia położenie geograficzne dziecka.
+	 * 
+	 * @param idChild indywidualny numer dziecka
+	 * @param point punkt z danymi o położeniu geograficznym
+	 * @return prawda - jeśli operacja się udała, fałsz - jeśli zakończono niepowodzeniem
+	 */
+	public boolean setLocation(int idChild, AreaPoint point);
+	//###
+	
+	//### STEP TIME ###
+	/**
+	 * Ustawia czas odstępu między sprawdzaniem położenia geograficznego
+	 * dla konkretnego dziecka.
+	 *  
+	 * @param idChild indywidualny numer dziecka
+	 * @param stepTime odstęp czasu między sprawdzaniem położenia
+	 * @return prawda - jeśli operacja się udała, fałsz - jeśli zakończono niepowodzeniem
+	 */
+	public boolean setStepTime(int idChild, float stepTime);
+	
+	/**
+	 * Pobiera czas odstępu między sprawdzaniem położenia geograficznego
+	 * dla konkretnego dziecka.
+	 * 
+	 * @param idChild indywidualny numer dziecka
+	 * @return prawda - jeśli operacja się udała, fałsz - jeśli zakończono niepowodzeniem
+	 */
+	public float getStepTime(int idChild);
+	//###
+	
+	//### NAME ###
+	/**
+	 * Ustawia nowe imię dziecka.
+	 * 
+	 * @param idChild indiwydualny numer dziecka
+	 * @param name imię dziecka
+	 * @return prawda - jeśli operacja się udała, fałsz - jeśli zakończono niepowodzeniem 
+	 */
+	public boolean setName(int idChild, String name);
+	
+	/**
+	 * Pobiera imię konkretnego dziecka.
+	 * 
+	 * @param idChild indiwydualny numer dziecka
+	 * @return napis zawierjący imię dziecka
+	 */
+	public String getName(int idChild);
+	//###
+	
+	//### IMEI ###
+	/**
+	 * Pobiera numer IMEI konkretnego dziecka.
+	 * 
+	 * @param idChild indiwydualny numer dziecka
+	 * @return napis zawierający numer IMEI
+	 */
+	public String getImei(int idChild);
+	
+	/**
+	 * Rejestruje numer IMEI konkretnego dziecka i zwraca wygenerowany,
+	 * unikalny klucz potrzebny do rejestracji.
+	 * 
+	 * @param idChild indiwydualny numer dziecka
+	 * @param imei numer IMEI
+	 * @return napis zawierający unikalny klucz
+	 */
+	public String registerImei(int idChild, String imei);
+	//###
+	
+	//### KEY ###
+	/**
+	 * Pobiera unikalny klucz dla konkretnego dziecka.
+	 * 
+	 * @param idChild indiwydualny numer dziecka
+	 * @return napis zawierający unikalny klucz
+	 */
+	public String getKey(int idChild);
+	//###
+	
+	//### BIRTH DATE###
+	/**
+	 * Ustawia datę urodzenia konkretnego dziecka.
+	 * 
+	 * @param idChild indiwydualny numer dziecka
+	 * @param date data urodzenia dziecka
+	 * @return prawda - jeśli operacja się udała, fałsz - jeśli zakończono niepowodzeniem
+	 */
+	public boolean setDateOfBirth(int idChild, Date date);
+	
+	/**
+	 * Pobiera datę urodzenia konkretnego dziecka.
+	 * 
+	 * @param idChild indiwydualny numer dziecka
+	 * @return data urodzenia dziecka
+	 */
+	public Date getDateOfBirth(int idChild);
+	//###
+	
+	//### LOCATION HISTORY ###
+	/**
+	 * Pobiera listę punktów geograficznych w których ostatnio przebywało dziecko.
+	 * Metoda pozwala określić limit ilości punktów.
+	 * 
+	 * @param idChild indiwydualny numer dziecka
+	 * @param historyLimit limit ilości zwracanych punktów
+	 * @return lista punktów geograficznych w któych przebywało dziecko
+	 */
+	public List<AreaPoint> getLocationHistory( int idChild, int historyLimit);
+	
+	/**
+	 * Pobiera listę punktów geograficznych w których ostatnio przebywało dziecko.
+	 * Metoda pozwala określić zakres czasowy.
+	 * 
+	 * @param idChild indiwydualny numer dziecka
+	 * @param dateLimit data od której sprawdzono historię położeń geograficznych
+	 * @return lista punktów geograficznych w któych przebywało dziecko
+	 */
+	public List<AreaPoint> getLocationHistory( int idChild, Date dateLimit);
+	//###
+}
